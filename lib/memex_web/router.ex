3fade69e0d0578d25707e9aa47cae934a66e43ea
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2024-08-01 10:28:41 -0300

    Add resources /memes, MemeController to router

--- lib/memex_web/router.ex
+++ lib/memex_web/router.ex
@@ -18,6 +18,7 @@ defmodule MemexWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/memes", MemeController
   end
 
   # Other scopes may use custom stacks.
