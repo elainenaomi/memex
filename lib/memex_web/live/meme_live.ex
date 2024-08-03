56b9d4ba59307e6d76e100675ef305d17d06f8e3
Elaine Watanabe <elaine.n.watanabe@gmail.com>
2024-08-02 23:50:37 -0300

    Ajustando o estilo da busca

--- lib/memex_web/live/meme_live.ex
+++ lib/memex_web/live/meme_live.ex
@@ -6,23 +6,22 @@ defmodule MemexWeb.MemeLive do
 
   def render(assigns) do
     ~H"""
-    <div>
-      <h1>Search Memes</h1>
-      <form phx-submit="search">
-        <input type="text" name="query" value={@query} placeholder="Search for memes..."/>
-        <button type="submit">Search</button>
-      </form>
-      <ul>
-        <%= for meme <- @memes do %>
-          <li>
-            <strong><%= meme.name %></strong> - <%= meme.description %>
-            <%= if meme.link do %>
-              [<a href={meme.link} target="_blank">Link</a>]
-            <% end %>
-          </li>
-        <% end %>
-      </ul>
-    </div>
+      <div class="flex items-center flex-col">
+        <form phx-submit="search">
+          <input type="text" name="query" value={@query} placeholder="Search for memes..."/>
+          <button type="submit">Search</button>
+        </form>
+        <ul class="pt-5">
+          <%= for meme <- @memes do %>
+            <li>
+              <strong><%= meme.name %></strong> - <%= meme.description %>
+              <%= if meme.link do %>
+                [<a href={meme.link} target="_blank">Link</a>]
+              <% end %>
+            </li>
+          <% end %>
+        </ul>
+      </div>
     """
   end
 
